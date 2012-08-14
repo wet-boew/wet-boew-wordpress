aae594cd4d2301e5223e23683ceb110f9ed138a5
Jobe Roberts <joberrr@gmail.com>
2012-08-14 17:41:53 -0300

    Update wet-boew3/sidebar.php

--- wet-boew3/sidebar.php
+++ wet-boew3/sidebar.php
@@ -33,36 +33,7 @@ if ($clf_col_num=='1') {
 			</div>
 		</nav>
 		</div></div>
-		<!-- Side navigation (left column) ends / Fin de la navigation latérale (colonne gauche) -->
-		
-		<!-- Supplemental content (right column) begins / Début du contenu supplémentaire (colonne droite) -->
-		<div id="cn-right-col-gap"></div>
-		<div id="cn-right-col" role="complementary"><div id="cn-right-col-inner">
-		<section>
-			<ul class="cn-right-col-default">
-		  
-		<?php   /* Widgetized sidebar, if you have the plugin installed. */ 
-				if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('right_column') ) : ?>
-				
-			<h2 id="cn-nav"><?php _e("<!--:en-->Supplemental content (right column)<!--:--><!--:fr-->Navigation principale (colonne gauche)<!--:-->"); ?></h2>
-			<div class="cn-right-col-default">
-			<section>
-				<h3><?php _e("<!--:en-->Add Widgets<!--:--><!--:fr-->Ajouter des gadgets logiciels<!--:-->"); ?></h3>
-				<ul>
-				  <li>
-					<p><?php _e("<!--:en-->To hide this default menu, go to 'Appearance' and add widgets.<!--:--><!--:fr-->Pour cacher ce menu par défaut, aller à &laquo; Appearance &raquo; et ajouter des gadgets logiciels.<!--:-->"); ?></p>
-				  </li>
-				</ul>
-			</section>
-			</div>
-		<?php endif; ?>
-			</ul>
-		</section>
-		</div></div>
-		<!-- Supplemental content (right column) ends / Fin du contenu supplémentaire (colonne droite) -->
-	</div></div>
-	<!-- Columns end / Fin des colonnes -->
-	
+		<!-- 3 column page layout deprecated -->
 <?php
 } else {
   
