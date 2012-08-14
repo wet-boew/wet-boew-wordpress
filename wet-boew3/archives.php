e5d641dbdab806b344c61cba34fc8a1ead3ef3ad
Jobe Roberts <joberrr@gmail.com>
2012-08-14 15:42:45 -0300

    update skip to content anchor: cn-cont to wb-cont

--- wet-boew3/archives.php
+++ wet-boew3/archives.php
@@ -4,7 +4,7 @@
     <!-- MainContentStart -->
 			
 			<!-- Content title begins / Début du titre du contenu -->
-			<h1 id="cn-cont"><?php the_title(); ?></h1>
+			<h1 id="wb-cont"><?php the_title(); ?></h1>
 			<!-- Content Title ends / Fin du titre du contenu -->
 			
 <!-- clf2-nsi2 theme begins / Début du thème clf2-nsi2 -->
