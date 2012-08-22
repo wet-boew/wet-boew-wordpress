c0733713f3bbb556775af04c8ac9af3d6267fb71
Jobe Roberts <joberrr@gmail.com>
2012-08-22 18:40:12 -0400

    adding new files

---
+++ wet-boew3/build/js/settings.js
@@ -0,0 +1,23 @@
+/*!
+ * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
+ * www.tbs.gc.ca/ws-nw/wet-boew/terms / www.sct.gc.ca/ws-nw/wet-boew/conditions
+ */
+/*
+WET-BOEW-Settings
+ */
+var wet_boew_properties = {
+	/** global plugins are called via a array of dependency names **/
+	globals : ['equalize', 'deselectradio', 'css3ie'/*, 'toolbar'*/]
+};
+
+/*
+Global overrides for individual components
+ */
+
+// Share widget
+var wet_boew_share = {
+	sites : ['del.icio.us', 'digg', 'facebook', 'google', 'linkedin', 'reddit', 'stumbleupon', 'twitter', 'yahoobuzz']
+};
+
+// Charts widget 
+// var wet_boew_charts = { };
