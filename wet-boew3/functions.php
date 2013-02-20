e6e9ecaa5e9b861f0e899ce0a06f5c8dd05dc93d
Jobe Roberts <joberrr@gmail.com>
2013-02-20 14:19:50 -0500

    Update wet-boew3/functions.php

--- wet-boew3/functions.php
+++ wet-boew3/functions.php
@@ -150,7 +150,7 @@ function the_breadcrumb($post) {
                 $anc = get_post_ancestors( $post->ID );
                 foreach ( $anc as $ancestor ) {
             		//wp_cache_delete($post->ID, 'posts');
-                    $output = '<li><a href="' . get_permalink($parent) . '">'.get_the_title($ancestor).'</li>'.$output;
+                    $output = '<li><a href="' . get_permalink($ancestor) . '">'.get_the_title($ancestor).'</li>'.$output;
                 }
                 echo $output;
                 echo '<li>';
