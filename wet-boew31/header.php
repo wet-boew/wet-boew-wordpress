4be2d8583766c52f5f1bb6f01762309e3d91c5da
A W <anthony@wilkinsonsignature.com>
2013-03-12 17:13:59 -0300

    Update header.php
    line 116 - files included for menu have .txt extension, not .inc

--- wet-boew31/header.php
+++ wet-boew31/header.php
@@ -113,7 +113,7 @@ if ($clf_col_num=='1') {
 
 <nav role="navigation">
 <div id="gcwu-psnb"><h2><?php _e("<!--:en--><span>Site </span>menu</h2><!--:--><!--:fr-->Menu<span> du site</span><!--:-->"); ?></h2><div id="gcwu-psnb-in"><div class="wet-boew-menubar mb-mega"><div>
-<ul class="mb-menu" data-ajax-replace="<?php bloginfo('template_directory'); ?>/demos/includes/menu-<?php _e("<!--:en-->eng<!--:--><!--:fr-->fra<!--:-->"); ?>.inc">
+<ul class="mb-menu" data-ajax-replace="<?php bloginfo('template_directory'); ?>/demos/includes/menu-<?php _e("<!--:en-->eng<!--:--><!--:fr-->fra<!--:-->"); ?>.txt">
 <li><section><h3><a href="#">Section 1</a></h3></section></li>
 <li><section><h3><a href="#">Section 2</a></h3></section></li>
 <li><section><h3><a href="#">Section 3</a></h3></section></li>
