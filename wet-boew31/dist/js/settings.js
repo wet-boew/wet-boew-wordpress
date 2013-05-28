5d838c97ca5420e80685f9eb65a3abaa039cacba
Jobe Roberts <joberrr@gmail.com>
2013-05-28 09:37:35 -0400

    Updating CSS to 3.1.1

--- wet-boew31/dist/js/settings.js
+++ wet-boew31/dist/js/settings.js
@@ -3,14 +3,14 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.io/wet-boew/License-eng.txt / wet-boew.github.io/wet-boew/Licence-fra.txt
  *
- * Version: v3.1.0-release Build: 2013-04-11 01:18 PM
+ * Version: v3.1.1-release Build: 2013-05-16 04:34 AM
  *
  */
 /*
 WET-BOEW-Settings
  */
 var wet_boew_properties = {
-	/** global plugins are called via a array of dependency names **/
+	/** global plugins are called via an array of dependency names **/
 	globals : ['deselectradio', 'datemodified']
 };
 
@@ -22,6 +22,3 @@ Global overrides for individual components
 var wet_boew_share = {
 	sites : ['del.icio.us', 'digg', 'facebook', 'google', 'linkedin', 'reddit', 'stumbleupon', 'twitter', 'yahoobuzz']
 };
-
-// Charts widget 
-// var wet_boew_charts = { };
