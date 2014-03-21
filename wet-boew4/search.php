da269ba61cb520460a3119c3f1ad9622d1e7484d
Jobe Roberts <joberrr@gmail.com>
2014-03-21 15:22:42 -0400

    Update search.php
    fixed formatting

--- wet-boew4/search.php
+++ wet-boew4/search.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
-	<div id="wb-core"><div id="wb-core-in" class="equalize">
-    <div id="wb-main" role="main"><div id="wb-main-in">
+
+<div class="container">
+<div class="row">
+
+<main role="main" property="mainContentOfPage" class="col-md-9 col-md-push-3">
     <!-- MainContentStart -->
 	<?php if (have_posts()) : ?>
 
@@ -32,15 +35,16 @@
 			<p class="center"><?php _e("<!--:en-->Sorry, no posts matched your criteria.<!--:--><!--:fr-->Désolé, aucun article ne correspond à vos critères.<!--:-->"); ?></p>
 	<?php endif; ?>
     <!-- Date Modified begins / Début de la date de modification -->
-			<dl id="gcwu-date-mod" role="contentinfo">
-				<dt><?php _e("<!--:en-->Date modified: <!--:--><!--:fr-->Date de modification&#160;:<!--:-->"); ?></dt>
-				<dd><span><time><?php the_time('Y-m-d') ?></time></span></dd>
-			</dl>
-			<div class="clear"></div>
-			<!-- Date Modified ends / Fin de la date de modification -->
-<!-- clf2-nsi2 theme ends / Fin du thème clf2-nsi2 -->
-		</div></div>
+			<!-- Date Modified begins / Début de la date de modification -->
+			<dl id="wb-dtmd" role="contentinfo" property="dateModified">
+            <dt><?php _e("<!--:en-->Date modified: <!--:--><!--:fr-->Date de modification&#160;:<!--:-->"); ?></dt> 
+                 <dd>
+                   <time><?php the_time('Y-m-d') ?></time>
+                 </dd>
+            </dl>
+		</main>
 		<!-- Main content ends / Fin du contenu principal --> 
 
 <?php get_sidebar(); ?>
+</div></div>
 <?php get_footer(); ?>
