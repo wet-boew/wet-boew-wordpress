0e0c58d0aa7c51b46a5773b99a2342316c92e0de
Jobe Roberts <joberrr@gmail.com>
2014-08-13 15:41:58 -0400

    Update header.php to 4.0.4

--- wet-boew4/header.php
+++ wet-boew4/header.php
@@ -105,10 +105,9 @@
                             </li>
                         </ul>
                         <section class="wb-mb-links col-xs-12 visible-sm visible-xs" id="wb-glb-mn">
-                            <h2>Menu</h2> 
+                            <h2><?php _e("<!--:en-->Search and menus<!--:--><!--:fr-->Recherche et menus<!--:-->"); ?></h2>
                             <ul class="pnl-btn list-inline text-right">
-                                <li><a href="#mb-pnl" title="Menu" aria-controls="mb-pnl" class="overlay-lnk btn btn-xs btn-default" role="button"><span class="glyphicon glyphicon-th-list"><span class="wb-inv">Menu</span></span></a>
-                                </li>
+<li><a href="#mb-pnl" title="Search and menus" aria-controls="mb-pnl" class="overlay-lnk btn btn-sm btn-default" role="button"><span class="glyphicon glyphicon-search"><span class="glyphicon glyphicon-th-list"><span class="wb-inv"><?php _e("<!--:en-->Search and menus<!--:--><!--:fr-->Recherche et menus<!--:-->"); ?></span></span></span></a></li>
                             </ul>
                             <div id="mb-pnl"></div>
                         </section>
